711ccad02832e682ea9afa1f92b9d4a93df76307
andres vargas <andressx1996@gmail.com>
2026-09-16 17:09:36 +0200

    task terminada, texto animado con clase .text-animate

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("Hola")
+window.addEventListener("load", function(){
+    
+    this.textos = document.getElementsByClassName("text-animate");
+
+    let margin = 0;
+    let marginMove = 10;
+    
+    setInterval(function() {
+        for(let i = 0 ; i < this.textos.length ; i++){
+            this.textos[i].style.marginLeft = margin + "px";
+        }
+
+        margin = margin + marginMove
+    }, 1000);
+
+});
